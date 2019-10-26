821508690b48fd6575c5e0cc55bccc14a83eb367
Aurélien VALADE <wolvi.lataniere@gmail.com>
2019-10-26 18:34:41 +0200

    adding push button action

--- joystick/src/main.py
+++ joystick/src/main.py
@@ -1,8 +1,15 @@
 from threading import Thread
 from sense_hat import SenseHat
 from mqttclient import MqttClient
+import json
 
 TOPIC='raw/joystick'
+R = [255,0,0]
+O = [255,127,0]
+Y = [255,255,0]
+G = [0,255,0]
+B = [0,0,255]
+N = [0,0,0]
 
 sense = SenseHat()
 try:
@@ -21,3 +28,13 @@ if __name__ == '__main__':
       if event.action == "pressed":
          print("Event " + event.direction)
          mqtt.client.publish(TOPIC, event.direction)
+         if event.direction == "middle":
+            mqtt.client.publish("disp/button", json.dumps({"image":
+[N, N, N, B, B, N, N, N,
+N, N, B, B, B, B, N, N,
+N, B, B, B, B, B, B, N,
+O, O, B, B, B, B, Y, Y,
+O, O, O, B, B, Y, Y, Y,
+O, O, O, O, Y, Y, Y, Y,
+N, O, O, O, Y, Y, Y, N,
+N, N, O, O, Y, Y, N, N]}))
